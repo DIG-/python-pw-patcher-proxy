44f4afcdf519d21196d70fca2d69460c594abd35
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2023-12-29 09:54:47 -0300

    Created basic response

--- src/dig_pw_patcher_proxy/proxy.py
+++ src/dig_pw_patcher_proxy/proxy.py
@@ -1,4 +1,6 @@
+from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from typing import Any
 
 from .cache import Cache
 from .downloader import Downloader
@@ -6,7 +8,20 @@ from .log import Log
 
 
 class _Handler(BaseHTTPRequestHandler):
-    pass
+    def __init__(self, *args, cache: Cache, downloader: Downloader, server: str, **kwargs):
+        self.cache = cache
+        self.downloader = downloader
+        self.original_url = server
+        self.log = Log.get("proxy")
+        super().__init__(*args, **kwargs)
+
+    def log_message(self, format: str, *args: Any):  # pylint: disable=redefined-builtin
+        self.log.debug(f"{self.address_string()} - {format % args}")
+
+    def do_GET(self):  # pylint: disable=invalid-name
+        self.send_response(HTTPStatus.NOT_FOUND)
+        self.send_header("Content-Length", "0")
+        self.end_headers()
 
 
 class Proxy:
@@ -17,7 +32,10 @@ class Proxy:
         self.port = port
         self.log = Log.get("proxy")
         self.log.info(f"Initialize server on 127.0.0.1:{port}")
-        self.daemon = ThreadingHTTPServer(server_address=("127.0.0.1", port), RequestHandlerClass=_Handler)
+        self.daemon = ThreadingHTTPServer(server_address=("127.0.0.1", port), RequestHandlerClass=self._handler)
 
     def run(self):
         self.daemon.serve_forever()
+
+    def _handler(self, *args, **kwargs) -> _Handler:
+        return _Handler(*args, cache=self.cache, downloader=self.downloader, server=self.server, **kwargs)
