af4fd4e7b3bb248c36bcd12196f888cf0aea749c
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2023-12-29 10:48:40 -0300

    Sample structure of update

--- src/dig_pw_patcher_proxy/downloader.py
+++ src/dig_pw_patcher_proxy/downloader.py
@@ -8,3 +8,15 @@ class Downloader:
         self.server = server
         self.jobs = jobs
         self.log = Log.get("downloader")
+
+
+# GET /element/version
+# GET /element/v-2.inc
+# GET /element/element/ZGF0YQ==/Z3Nob3AuZGF0YQ==
+#
+# v-*.inc file:
+# # v-from v-to bytes
+# !hash /absolute/path/to/file
+# !hash relative/to/last/file
+# !hash /new/absolute/file
+# -----BEGIN ELEMENT SIGNATURE-----
