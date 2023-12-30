6b7fd31d76bbb0f2861b625dd1db0b25134b7335
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2023-12-30 09:29:35 -0300

    Created external default Arguments

--- src/dig_pw_patcher_proxy/arguments.py
+++ src/dig_pw_patcher_proxy/arguments.py
@@ -17,15 +17,26 @@ class Arguments:
 
     @staticmethod
     def parse() -> "Arguments":
+        default = Arguments.default()
         parser = ArgumentParser(prog="dig_pw_patcher_proxy", description="PW Patcher Proxy")
-        parser.add_argument(
-            "-s", "--server", default="http://fpatch3.perfectworld.com.br/CPW/", help="Original patch server url"
-        )
-        parser.add_argument("-c", "--cache", default="cache", type=Path, help="Directory to cache files")
+        parser.add_argument("-s", "--server", default=default.server, help="Original patch server url")
+        parser.add_argument("-c", "--cache", default=default.cache, type=Path, help="Directory to cache files")
         parser.add_argument("--clear-cache", action="store_true", help="Clear entire cache before start")
-        parser.add_argument("-j", "--jobs", default=8, type=int, help="Number of parallel cache download")
-        parser.add_argument("--bind", default="127.0.0.1", type=str, help="Bind proxy server to")
-        parser.add_argument("-p", "--port", default=8080, type=int, help="Port of proxy server")
-        parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase log level")
+        parser.add_argument("-j", "--jobs", default=default.jobs, type=int, help="Number of parallel cache download")
+        parser.add_argument("--bind", default=default.bind, type=str, help="Bind proxy server to")
+        parser.add_argument("-p", "--port", default=default.port, type=int, help="Port of proxy server")
+        parser.add_argument("-v", "--verbose", action="count", default=default.verbose, help="Increase log level")
         args = parser.parse_args()
         return Arguments(args.server, args.cache, args.jobs, args.bind, args.port, args.verbose, args.clear_cache)
+
+    @staticmethod
+    def default() -> "Arguments":
+        return Arguments(
+            server="http://fpatch3.perfectworld.com.br/CPW/",
+            cache=Path("cache"),
+            jobs=8,
+            bind="127.0.0.1",
+            port=8080,
+            verbose=0,
+            clear_cache=False,
+        )
